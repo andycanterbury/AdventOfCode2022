910f0ea8b666fc5b0151d2f184c6b22108376176
andycanterbury <andrew.canterbury@ingagepartners.com>
2022-11-30 22:48:30 -0500

    Initial commit

---
+++ Day1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var fileData = Utilities.InputLoader.LoadFile("data.txt");
+
+Console.WriteLine(fileData[0]);
